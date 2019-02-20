3b113622221cee83cdcb9ae890c1c9dc0a64df74
Unknown <schewshu@purdue.edu>
2019-02-20 16:42:32 -0500

    automation testing bug fixed

--- WeChefApp/e2e/toolsTest.spec.js
+++ WeChefApp/e2e/toolsTest.spec.js
@@ -63,13 +63,13 @@ describe('Tools', () => {
 
 
   /* Volumn */
-  let l = '100';
-  let ml = (parseInt(l,10) * 1000).toFixed(2);
-  let gal = (parseInt(l,10) / 3.78541).toFixed(2);
-  let fl_oz = (parseInt(l,10) / 3.78541 * 128).toFixed(2);
-  let cup = (parseInt(l,10) / 3.78541 * 128 * 8).toFixed(2);
-  let tsp = (parseInt(l,10) * 202.8841).toFixed(2);
-  let Tbs = (parseInt(l,10) * 202.8841 / 3).toFixed(2);
+  let l = 1;
+  let ml = (parseInt(l,10) * 1000);
+  let gal = (parseInt(l,10) / 3.78541);
+  let fl_oz = (parseInt(l,10) / 3.78541 * 128);
+  let cup = (parseInt(l,10) / 3.78541 * 128 / 8);
+  let tsp = (parseInt(l,10) * 202.8841);
+  let Tbs = (parseInt(l,10) * 202.8841 / 3);
 
   function volConversionTest(unit1, unit2, unit1o, unit2o) {
     it('Testing Volume '+unit1+'-'+unit2, async () => {
@@ -81,19 +81,19 @@ describe('Tools', () => {
     await element(by.text(unit1)).atIndex(0).tap();
 
     await element(by.id('inputAmountTextField')).tap();
-    await element(by.id('inputAmountTextField')).typeText(unit1o);
+    await element(by.id('inputAmountTextField')).typeText(unit1o.toFixed(4));
 
     await element(by.label('Convert Unit')).tap();
     await element(by.text(unit2)).atIndex(0).multiTap(3);
 
-    await expect(element(by.text(unit2o))).toBeVisible();
+    await expect(element(by.text((unit2o.toFixed(2))))).toBeVisible();
     });
   }
 
 
   volConversionTest('gal','l',gal,l);
-  volConversionTest('cup','ml',cup,ml);
-  volConversionTest('tsp','fl-oz',tsp,fl_oz);
+  volConversionTest('tsp','ml',tsp,ml);
+  volConversionTest('cup','fl-oz',cup,fl_oz);
   volConversionTest('Tbs','gal',Tbs,gal);
 
 
