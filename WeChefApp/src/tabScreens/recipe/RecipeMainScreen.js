feb1f38d16d348338b9660431310f9bdedb87eff
Ryan Fan <fanzuyuan1996@gmail.com>
2019-03-18 23:49:19 -0400

    Search bar done with animations

--- WeChefApp/src/tabScreens/recipe/RecipeMainScreen.js
+++ WeChefApp/src/tabScreens/recipe/RecipeMainScreen.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Animated,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
+// components
+import SearchBar from '../components/SearchBar';
+
 class RecipeMainScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.animatedValue = new Animated.Value(0);
+
+    this.state = {
+      isFocusedOnTouchBar: false,
+    };
+
+    this.searchBarExpand = this.searchBarExpand.bind(this);
+  }
+
   componentDidMount() {
     this._navListener = this.props.navigation.addListener('didFocus', () => {
       StatusBar.setBarStyle('light-content');
@@ -14,21 +39,100 @@ class RecipeMainScreen extends Component {
     this._navListener.remove();
   }
 
-  render() {
+  searchBarExpand(focused) {
+    if (!focused && this.state.isFocusedOnTouchBar === true) {
+      this.animatedValue.setValue(1);
+
+      Animated.timing(
+        this.animatedValue,
+        {
+          toValue: 0,
+          duration: 200,
+        }
+      ).start();
+
+      this.setState({ isFocusedOnTouchBar: false });
+    }
+    else if (focused && this.state.isFocusedOnTouchBar === false) {
+      this.animatedValue.setValue(0);
+
+      Animated.timing(
+        this.animatedValue,
+        {
+          toValue: 1,
+          duration: 200,
+        }
+      ).start();
+
+      this.setState({ isFocusedOnTouchBar: true });
+    }
+  }
+
+
+  searchBarFocusCallback = focused => {
+    this.searchBarExpand(focused);
+  }
+
+  renderCancelButton() {
     return (
-      <View style={styles.container}>
-        <LinearGradient
-          start={{x: 0, y: 0}} end={{x: 1, y: 1}}
-          colors={['#FA967B', '#F56862']}
-          style={styles.searchBarContainer}
-        >
+      <TouchableOpacity
+        style={styles.cancelButtonContainer}
+        onPress={() => {
+          this.searchBarExpand(false);
+          this.refs.childrenSearchBarRef.refs.searchBarRef.blur();
+          this.refs.childrenSearchBarRef.focusedAnimation(false);
+        }}
+      >
+        <Text style={styles.cancelText}>Cancel</Text>
+      </TouchableOpacity>
+    );
+  }
 
-        </LinearGradient>
+  render() {
+    const endHeight = Dimensions.get('window').height;
+    const animatedHeight = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [getStatusBarHeight() + 124, endHeight]
+    });
 
-        <View>
-          <Text>Content</Text>
+    return (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Animated.View
+            style={{
+              width: '100%',
+              height: animatedHeight,
+            }}
+          >
+            <LinearGradient
+              start={{x: 0, y: 0}} end={{x: 1, y: 1}}
+              colors={['#FA967B', '#F56862']}
+              style={styles.searchBarContainer}
+            >
+              {/* <TouchableWithoutFeedback
+                onPressIn={() => this.searchBarExpand(!this.state.isFocusedOnTouchBar)}
+              > */}
+                <View style={styles.touchContainer}>
+                  <SearchBar
+                    callbackFromParent={this.searchBarFocusCallback}
+                    ref="childrenSearchBarRef"
+                  />
+                  {
+                    this.state.isFocusedOnTouchBar
+                    ? this.renderCancelButton()
+                    : <View></View>
+                  }
+
+                </View>
+              {/* </TouchableWithoutFeedback> */}
+            </LinearGradient>
+          </Animated.View>
+
+          <View>
+            <Text>Content</Text>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -43,7 +147,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
   },
   searchBarContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 80,
+
     width: '100%',
-    height: getStatusBarHeight() + 120,
+    // height: getStatusBarHeight() + 124,
   },
+  touchContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    width: '100%',
+  },
+  cancelButtonContainer: {
+    width: '13%',
+    height: 48,
+    // backgroundColor: '#000',
+    right: '6%',
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cancelText: {
+    color: '#FFF',
+    fontSize: 13,
+  }
 });
