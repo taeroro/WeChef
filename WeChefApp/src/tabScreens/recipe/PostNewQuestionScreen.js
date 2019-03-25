bf7c758483a1936741e4dd668da838de901896ef
Unknown <meng46@purdue.edu>
2019-03-25 16:56:21 -0400

    Completed post question

--- WeChefApp/src/tabScreens/recipe/PostNewQuestionScreen.js
+++ WeChefApp/src/tabScreens/recipe/PostNewQuestionScreen.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, Dimensions, TouchableOpacity} from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+const sectionSize = Dimensions.get('window').width - 40;
 
 class PostNewQuestionScreen extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: '',
+    };
+  }
+
   componentDidMount() {
     this._navListener = this.props.navigation.addListener('didFocus', () => {
       StatusBar.setBarStyle('dark-content');
@@ -12,10 +24,31 @@ class PostNewQuestionScreen extends Component {
     this._navListener.remove();
   }
 
+  handleSubmit = () => {
+    console.log(this.state.question);
+    
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>PostNewQuestionScreen</Text>
+        <TextInput
+          label = 'Type your question here'
+          value = {this.state.question}
+          onChangeText= {question => this.setState({question})}
+          multiline
+          scrollEnabled
+          style = {styles.textInput}
+          underlineColor = '#F56862'
+        />
+
+        <TouchableOpacity
+          onPress={() => this.handleSubmit()}
+          style={styles.submitContainer}
+        >
+          <Text style={styles.submitText} >Submit</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
@@ -30,4 +63,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#FFF',
   },
+  textInput: {
+    width: '80%',
+    position: 'absolute',
+    top: 10,
+    bottom: 500
+  },
+  submitContainer: {
+    marginTop: -100,
+    width: sectionSize,
+    alignItems: 'center',
+  },
+  submitText: {
+    fontSize: 15,
+    fontFamily: 'Poppins',
+    fontWeight: '500',
+    color: '#F56862',
+  }
 });
