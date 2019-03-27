b551db0df1310c805057f1aa4077162c3b0ba919
Ryan Fan <fanzuyuan1996@gmail.com>
2019-03-27 00:13:42 -0400

    directions done

--- WeChefApp/src/tabScreens/recipe/topTabBar/DirectionsScreen.js
+++ WeChefApp/src/tabScreens/recipe/topTabBar/DirectionsScreen.js
@@ -3,28 +3,47 @@ import {
   StyleSheet,
   View,
   Text,
-  Image,
+  ScrollView,
+  Dimensions,
 } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+const sectionSize = Dimensions.get('window').width - 40;
 
 class DirectionsScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      directions: [],
     };
   }
 
-  /*
-  * TODO: display directions
-  * Backend data stored in params.content.
-  * e.g. ["Put sugar into milk", "Grind mix of ice and milk"]
-  */
+  componentDidMount() {
+    this.setState({ directions: this.props.navigation.state.params.content });
+  }
 
   render() {
-    console.log(this.props.navigation.state.params.content);
     return (
       <View style={styles.container}>
-        <Text>DirectionsScreen</Text>
+        <ScrollView
+          contentContainerStyle={styles.scrollContainer}
+          style={{width: "100%"}}
+        >
+          {this.state.directions.map((item, i) => {
+            return (
+              <View style={styles.singleStep} key={i}>
+                <View style={styles.stepNumContainer}>
+                  <Text style={[styles.stepText, styles.stepN]}>N° </Text>
+                  <Text style={[styles.stepText, styles.stepNum]}>{i+1}</Text>
+                </View>
+                <Text style={[styles.stepText, styles.stepContent]}>{item}</Text>
+
+                <View style={styles.divider} />
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
     );
   }
@@ -37,5 +56,47 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingTop: 20,
+    paddingRight: 25,
+    paddingLeft: 25,
+    marginBottom: getBottomSpace(),
+  },
+  scrollContainer: {
+    flex: 0,
+    paddingBottom: 25,
+  },
+  singleStep: {
+
+  },
+  stepText: {
+    fontFamily: 'Poppins',
+  },
+  stepNumContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+
+  },
+  stepN: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#F56862',
+    paddingBottom: 8,
+  },
+  stepNum: {
+    fontSize: 36,
+    fontWeight: '600',
+    color: '#F56862',
+  },
+  stepContent: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#3C3C3C',
+  },
+  divider: {
+    marginTop: 20,
+    marginBottom: 20,
+    height: 0.8,
+    width: sectionSize,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
   },
 });
