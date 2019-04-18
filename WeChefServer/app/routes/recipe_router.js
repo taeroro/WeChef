01d4a502fd7af6ed5faad078b09601d1c430aba8
mush-zhang <junelena1313@gmail.com>
2019-04-17 23:58:45 -0400

    test case for get recipe by id/facebookid, modify  create recipe and find recipe by owner

--- WeChefServer/app/routes/recipe_router.js
+++ WeChefServer/app/routes/recipe_router.js
@@ -11,56 +11,71 @@ let ImageUpload = require('../middlewares/image_parser')
 let recipeRouter = express.Router();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUD_API_KEY,
+	api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // see my recipes
 recipeRouter.get('/by-owner/:facebookID', (req, res, err) => {
+	User.findOne({ facebookID: req.params.facebookID, }, (err, user) => {
+		if (err){
 
-    let fbID = req.params.facebookID;
-    if (!fbID) {
-        return res.status(422).send({
-            message: 'No FacebookID provided.',
-        });
-    }
+			return res.status(500).send({
+				message: err,
+			});
+		}
 
-    Recipe.find({ ownerID: fbID, }, '_id title recipeImageURL difficulty', (err, recipes) => {
-        if (err){
-            return res.status(500).send({
-                message: err,
-            });
-        }
-        if (recipes) {
-            res.status(200).send(recipes);
-        } else {
-            return res.status(404).send({
-                message: 'No recipes for the given user id.',
-            });
-        }
-    });
+		if (user) {
+			Recipe.find({ ownerID: req.params.facebookID, }, '_id title recipeImageURL difficulty', (err, recipes) => {
+				if (err){
+
+					return res.status(500).send({
+						message: err,
+					});
+				}
+				if (recipes) {
+					res.status(200).send(recipes);
+				} else {
+					return res.status(404).send({
+						message: 'No recipes for the given user id.',
+					});
+				}
+			});
+					
+		} else {
+			return res.status(404).send({
+				message: 'No user exists with given id.',
+			});
+		}
+	});
 
 });
 
+// TESTED
 // get one recipe
 recipeRouter.get('/recipe-byid/:recipeID', (req, res, err) => {
 
-    Recipe.findById(req.params.recipeID, (err, recipe) => {
-        if (err){
+	Recipe.findById(req.params.recipeID, (err, recipe) => {
+		if (err){
 
-            return res.status(500).send({
-                message: err,
-            });
-        }
-        if (recipe) {
-            res.status(200).send(recipe);
-        } else {
-            return res.status(404).send({
-                message: 'No recipe exists with given id.',
-            });
-        }
-    });
+			if (err.name === 'CastError') {
+				return res.status(422).send({
+					message: 'Recipe ID is not valid.',
+				});
+			}
+			return res.status(500).send({
+				message: err,
+			});
+		}
+		if (recipe) {
+			res.status(200).send(recipe);
+		} else {
+			return res.status(404).send({
+				message: 'No recipe exists with given id.',
+			});
+		}
+	});
 
 });
 
@@ -71,286 +86,304 @@ recipeRouter.get('/recipe-byid/:recipeID', (req, res, err) => {
  * tokenization is handleded internally.
  */
 recipeRouter.get('/search', (req, res, err) => {
-    if (!req.query.keywords) {
-        return res.status(422).send({
-            message: 'No keywords provided.',
-        });
-    }
-    /**
-     * $text will tokenize the search string using whitespace and most punctuation
-     * as delimiters, and perform a logical OR of all such tokens in the search string.
-     */
-    Recipe.find({ '$text': { '$search': req.query.keywords, }},
-        { score: { '$meta': 'textScore', }})
-        .sort({ 'score': { '$meta': 'textScore', }})
-        .exec( (err, recipes) => {
-            if (err) {
-                return res.status(500).send({
-                    message: err,
-                });
-            }
-            // use res.json as the list might be empty
-            res.json(recipes);
-        });
+	if (!req.query.keywords) {
+		return res.status(422).send({
+			message: 'No keywords provided.',
+		});
+	}
+	/**
+	 * $text will tokenize the search string using whitespace and most punctuation
+	 * as delimiters, and perform a logical OR of all such tokens in the search string.
+	 */
+	Recipe.find({ '$text': { '$search': req.query.keywords, }},
+		{ score: { '$meta': 'textScore', }})
+		.sort({ 'score': { '$meta': 'textScore', }})
+		.exec( (err, recipes) => {
+			if (err) {
+				return res.status(500).send({
+					message: err,
+				});
+			}
+			// use res.json as the list might be empty
+			res.json(recipes);
+		});
 });
 
+// TESTED
 // create recipe
 recipeRouter.post('/create', ImageUpload.recipeImageUpload, (req, res, err) => {
-    let new_recipe = new Recipe();
-    new_recipe.title = req.body.title;
-    new_recipe.ownerID = req.body.ownerID;
-    new_recipe.content = req.body.content;
-    new_recipe.ingredients = req.body.ingredients;
-    new_recipe.difficulty = req.body.difficulty ? req.body.difficulty : 0 ;
-    if (req.file) {
-      new_recipe.recipeImageURL = req.file.url;
-      new_recipe.recipeImageID = req.file.public_id;
-    }
+	let new_recipe = new Recipe();
+	new_recipe.title = req.body.title;
+	new_recipe.ownerID = req.body.ownerID;
+	new_recipe.content = req.body.content;
+	new_recipe.ingredients = req.body.ingredients;
+	new_recipe.difficulty = req.body.difficulty ? req.body.difficulty : 0 ;
+	if (req.file) {
+	  new_recipe.recipeImageURL = req.file.url;
+	  new_recipe.recipeImageID = req.file.public_id;
+	}
 
-    new_recipe.save((err, recipe) => {
-      if (err) {
-        if (new_recipe.recipeImageID) {
-          cloudinary.v2.api.delete_resources(new_recipe.recipeImageID);
-        }
-        if (err.name === 'ValidationError') {
-            return res.status(422).send({
-                message: err.errors,
-            });
-        } else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
-            return res.status(409).send({
-                message: 'This recipe has already been created.',
-            });
-        } else {
-            return res.status(500).send({
-                message: err,
-            });
-        }
-      }
-      return res.status(201).send({
-          message: 'OK',
-          recipeID: new_recipe._id,
-          recipeImage: new_recipe.recipeImageURL
-      });
-    })
+	User.findOne({ facebookID: new_recipe.ownerID, }, (err, user) => {
+		if (err){
+
+			return res.status(500).send({
+				message: err,
+			});
+		}
+
+		if (user) {
+			new_recipe.save((err, recipe) => {
+				if (err) {
+					if (new_recipe.recipeImageID) {
+				  		cloudinary.v2.api.delete_resources(new_recipe.recipeImageID);
+					}
+					if (err.name === 'ValidationError') {
+						return res.status(422).send({
+							message: err.errors,
+						});
+					} else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
+						return res.status(409).send({
+							message: 'This recipe has already been created.',
+						});
+					} else {
+						return res.status(500).send({
+							message: err,
+						});
+					}
+				}
+				return res.status(201).send({
+				  	message: 'OK',
+				  	recipeID: new_recipe._id,
+				  	recipeImage: new_recipe.recipeImageURL
+				});
+			})
+
+		} else {
+			return res.status(404).send({
+				message: 'No user exists with given id.',
+			});
+		}
+	});
+
 });
 
 // edit recipe
 recipeRouter.put('/edit/:recipeID', ImageUpload.recipeImageUpload, (req, res, err) => {
 
-    Recipe.findById(req.params.recipeID, (err, recipe) => {
-        if (err){
-            return res.status(500).send({
-                message: err,
-            });
-        }
-        if (recipe) {
-            recipe.title = req.body.title;
-            recipe.content = req.body.content || [];
-            recipe.ingredients = req.body.ingredients || [];
-            recipe.difficulty = req.body.difficulty;
-            let prevImage = recipe.recipeImageID;
+	Recipe.findById(req.params.recipeID, (err, recipe) => {
+		if (err){
+			return res.status(500).send({
+				message: err,
+			});
+		}
+		if (recipe) {
+			recipe.title = req.body.title;
+			recipe.content = req.body.content || [];
+			recipe.ingredients = req.body.ingredients || [];
+			recipe.difficulty = req.body.difficulty;
+			let prevImage = recipe.recipeImageID;
 
-            if (req.file) {
-              recipe.recipeImageURL = req.file.url;
-              recipe.recipeImageID = req.file.public_id;
-            }
+			if (req.file) {
+			  recipe.recipeImageURL = req.file.url;
+			  recipe.recipeImageID = req.file.public_id;
+			}
 
-            recipe.save((err, recipe2) => {
-                if (err) {
-                    if (err.name === 'ValidationError') {
-                        return res.status(422).send({
-                            message: err.errors,
-                        });
-                    } else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
-                        return res.status(409).send({
-                            message: 'Violated value unique.',
-                        });
-                    } else {
-                        return res.status(500).send({
-                            errorType: 'InternalError',
-                            message: err,
-                        });
-                    }
-                }
-                if (req.file && prevImage) {
-                    cloudinary.v2.api.delete_resources([ prevImage, ]);
-                }
-                return res.status(200).send({
-                    message: 'OK',
-                });
-            });
-        } else {
-            return res.status(404).send({
-                message: 'No recipe exists with given recipeID.',
-            });
-        }
-    });
+			recipe.save((err, recipe2) => {
+				if (err) {
+					if (err.name === 'ValidationError') {
+						return res.status(422).send({
+							message: err.errors,
+						});
+					} else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
+						return res.status(409).send({
+							message: 'Violated value unique.',
+						});
+					} else {
+						return res.status(500).send({
+							errorType: 'InternalError',
+							message: err,
+						});
+					}
+				}
+				if (req.file && prevImage) {
+					cloudinary.v2.api.delete_resources([ prevImage, ]);
+				}
+				return res.status(200).send({
+					message: 'OK',
+				});
+			});
+		} else {
+			return res.status(404).send({
+				message: 'No recipe exists with given recipeID.',
+			});
+		}
+	});
 
 });
 
 // get all recipe for home page, sort by date
 recipeRouter.get('/homepage', (req, res, err) => {
 
-    Recipe.find({}, '_id title recipeImageURL difficulty', {sort: { updatedAt: -1 }}, (err, recipes) => {
-      if (err){
-          return res.status(500).send({
-              message: err,
-          });
-      }
-      if (recipes) {
-          res.status(200).send(recipes);
-      } else {
-          return res.status(404).send({
-              message: 'No recipes can be found.',
-          });
-      }
-    });
+	Recipe.find({}, '_id title recipeImageURL difficulty', {sort: { updatedAt: -1 }}, (err, recipes) => {
+	  if (err){
+		  return res.status(500).send({
+			  message: err,
+		  });
+	  }
+	  if (recipes) {
+		  res.status(200).send(recipes);
+	  } else {
+		  return res.status(404).send({
+			  message: 'No recipes can be found.',
+		  });
+	  }
+	});
 });
 
 // get my fav recipes
 recipeRouter.get('/:facebookID/favourite', (req, res, err) => {
-    User.findOne({ facebookID: req.params.facebookID }, (err, user) => {
-      const favouriteRecipeIDs = user.favoriteRecipeIDs;
-      Recipe.find({ _id: {$in: favouriteRecipeIDs} }, (err, recipes) => {
-        if (err){
-            return res.status(500).send({
-                message: err,
-            });
-        }
-        if (recipes) {
-            res.status(200).send(recipes);
-        } else {
-            return res.status(404).send({
-                message: 'No recipes can be found.',
-            });
-        }
-      })
-    })
+	User.findOne({ facebookID: req.params.facebookID }, (err, user) => {
+	  const favouriteRecipeIDs = user.favoriteRecipeIDs;
+	  Recipe.find({ _id: {$in: favouriteRecipeIDs} }, (err, recipes) => {
+		if (err){
+			return res.status(500).send({
+				message: err,
+			});
+		}
+		if (recipes) {
+			res.status(200).send(recipes);
+		} else {
+			return res.status(404).send({
+				message: 'No recipes can be found.',
+			});
+		}
+	  })
+	})
 });
 
 // Add Q&A to a recipe
 recipeRouter.post('/qa/create/:recipeID', (req, res, err) => {
-    let new_qAndA = new QAndA();
-    new_qAndA.qContent = req.body.qContent;
-    new_qAndA.qRecipeIDs = req.params.recipeID;
-    new_qAndA.qOwnerID = req.body.qOwnerID;
-    console.log(new_qAndA.qContent);
-    console.log(new_qAndA.qRecipeIDs);
-    console.log(new_qAndA.qOwnerID);
+	let new_qAndA = new QAndA();
+	new_qAndA.qContent = req.body.qContent;
+	new_qAndA.qRecipeIDs = req.params.recipeID;
+	new_qAndA.qOwnerID = req.body.qOwnerID;
+	console.log(new_qAndA.qContent);
+	console.log(new_qAndA.qRecipeIDs);
+	console.log(new_qAndA.qOwnerID);
 
-    new_qAndA.save((error, qAndA) => {
-      if (error) {
-        if (err.name === 'ValidationError') {
-            return res.status(422).send({
-                message: err.errors,
-            });
-        } else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
-            return res.status(409).send({
-                message: 'This Q and A has already been created.',
-            });
-        } else {
-            return res.status(500).send({
-                message: err,
-            });
-        }
-      }
-      return res.status(201).send({
-          message: 'OK',
-          qAndA: qAndA,
-      });
-    })
+	new_qAndA.save((error, qAndA) => {
+	  if (error) {
+		if (err.name === 'ValidationError') {
+			return res.status(422).send({
+				message: err.errors,
+			});
+		} else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
+			return res.status(409).send({
+				message: 'This Q and A has already been created.',
+			});
+		} else {
+			return res.status(500).send({
+				message: err,
+			});
+		}
+	  }
+	  return res.status(201).send({
+		  message: 'OK',
+		  qAndA: qAndA,
+	  });
+	})
 });
 
 // Get Q and A's of a recipe
 recipeRouter.get('/qa/:recipeID', (req, res, err) => {
-    let recipeID = req.params.recipeID;
+	let recipeID = req.params.recipeID;
 
-    if (!recipeID) {
-        return res.status(422).send({
-            message: 'No recipeID provided.',
-        });
-    }
+	if (!recipeID) {
+		return res.status(422).send({
+			message: 'No recipeID provided.',
+		});
+	}
 
-    QAndA.find({qRecipeIDs: recipeID}, {}, {sort: { createdAt: -1 }}, (err, qAndAs) => {
-      if (err){
-          console.log(err);
-          return res.status(500).send({
-              message: err,
-          });
-      }
-      if (qAndAs) {
-          res.status(200).send(qAndAs);
-      } else {
-          return res.status(404).send({
-              message: 'No qAndAs for the given recipe id.',
-          });
-      }
-    });
+	QAndA.find({qRecipeIDs: recipeID}, {}, {sort: { createdAt: -1 }}, (err, qAndAs) => {
+	  if (err){
+		  console.log(err);
+		  return res.status(500).send({
+			  message: err,
+		  });
+	  }
+	  if (qAndAs) {
+		  res.status(200).send(qAndAs);
+	  } else {
+		  return res.status(404).send({
+			  message: 'No qAndAs for the given recipe id.',
+		  });
+	  }
+	});
 });
 
 // Get the latest Q and A of a recipe
 recipeRouter.get('/qa/first/:recipeID', (req, res, err) => {
-    let recipeID = req.params.recipeID;
+	let recipeID = req.params.recipeID;
 
-    if (!recipeID) {
-        return res.status(422).send({
-            message: 'No recipeID provided.',
-        });
-    }
+	if (!recipeID) {
+		return res.status(422).send({
+			message: 'No recipeID provided.',
+		});
+	}
 
-    QAndA.find({qRecipeIDs: recipeID}, {}, {sort: { createdAt: -1 }, limit: 1}, (err, qAndA) => {
-      if (err){
-          return res.status(500).send({
-              message: err,
-          });
-      }
-      if (qAndA) {
-          res.status(200).send(qAndA);
-      } else {
-          return res.status(404).send({
-              message: 'No qAndAs for the given recipe id.',
-          });
-      }
-    });
+	QAndA.find({qRecipeIDs: recipeID}, {}, {sort: { createdAt: -1 }, limit: 1}, (err, qAndA) => {
+	  if (err){
+		  return res.status(500).send({
+			  message: err,
+		  });
+	  }
+	  if (qAndA) {
+		  res.status(200).send(qAndA);
+	  } else {
+		  return res.status(404).send({
+			  message: 'No qAndAs for the given recipe id.',
+		  });
+	  }
+	});
 });
 
 // Update the Q and A
 recipeRouter.put('/qa/answer/:QnAID', (req, res, err) => {
-      QAndA.findById(req.params.QnAID, (err, qna) => {
-        if (err) {
-            return res.status(500).send({
-                message: err,
-            });
-        }
-        if (qna) {
-            qna.aContent = req.body.aContent;
-            qna.save((err, qna2) => {
-              if (err) {
-                  if (err.name === 'ValidationError') {
-                      return res.status(422).send({
-                          message: err.errors,
-                      });
-                  } else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
-                      return res.status(409).send({
-                          message: 'Violated value unique.',
-                      });
-                  } else {
-                      return res.status(500).send({
-                          errorType: 'InternalError',
-                          message: err,
-                      });
-                  }
-              }
-              return res.status(200).send({
-                  message: 'OK',
-              });
-            })
-        } else {
-            return res.status(404).send({
-                message: 'No QnA exists with given QnAID.',
-            });
-        }
-      })
+	  QAndA.findById(req.params.QnAID, (err, qna) => {
+		if (err) {
+			return res.status(500).send({
+				message: err,
+			});
+		}
+		if (qna) {
+			qna.aContent = req.body.aContent;
+			qna.save((err, qna2) => {
+			  if (err) {
+				  if (err.name === 'ValidationError') {
+					  return res.status(422).send({
+						  message: err.errors,
+					  });
+				  } else if (err.name === 'BulkWriteError' || err.name === 'MongoError') {
+					  return res.status(409).send({
+						  message: 'Violated value unique.',
+					  });
+				  } else {
+					  return res.status(500).send({
+						  errorType: 'InternalError',
+						  message: err,
+					  });
+				  }
+			  }
+			  return res.status(200).send({
+				  message: 'OK',
+			  });
+			})
+		} else {
+			return res.status(404).send({
+				message: 'No QnA exists with given QnAID.',
+			});
+		}
+	  })
 })
 
 module.exports = recipeRouter;
--- WeChefServer/test/recipe_tests.js
+++ WeChefServer/test/recipe_tests.js
@@ -4,6 +4,7 @@ let sleep = require('sleep');
 let server = require('../server');
 let chaiHttp = require('chai-http');
 let Recipe = require('../app/models/recipe');
+let User = require('../app/models/user');
 let uuid = require('uuid/v1');
 let faker = require('faker');
 let should = chai.should();
@@ -12,10 +13,21 @@ chai.use(chaiHttp);
 
 describe('Recipe', function() {
 
-    let create_valid_recipe = () => {
+
+    let create_valid_user = () => {
+        let valid_user = {
+            userName: faker.internet.userName(),
+            facebookID: uuid(),
+            email: faker.internet.email(),
+            userImageURL: faker.internet.url(),
+        };
+        return valid_user;
+    }
+
+    let create_valid_recipe = (facebookID) => {
         let valid_recipe = {
             title: faker.commerce.productName(),
-            ownerID: uuid(),
+            ownerID: facebookID,
             recipeImageURL: faker.internet.url(),
             userImageURL: faker.internet.url(),
             difficulty: faker.random.number(),
@@ -28,11 +40,11 @@ describe('Recipe', function() {
 
     this.timeout(400000);
 
-    // Setup for tests
+    // Setup for tests_id_id_id
 
     before(done => {
 
-        Recipe.remove({}, err => {
+        Recipe.deleteMany({}, err => {
 
             for(let i = 0; i < 10; i++) {
                 recipe_list.push(create_valid_recipe());
@@ -54,7 +66,7 @@ describe('Recipe', function() {
 
     after(done => {
 
-        Recipe.remove({}, err => {
+        Recipe.deleteMany({}, err => {
             done();
         });
     });
@@ -62,82 +74,150 @@ describe('Recipe', function() {
     describe('/POST Recipe Creation', () => {
 
         it('it should reject with no ownerID provided', done => {
-            let recipe = create_valid_recipe();
+            let valid_user = create_valid_user();
+            let recipe = create_valid_recipe('na');
             delete recipe.ownerID;
 
             chai.request(server)
                 .post('/recipe/create')
                 .send(recipe)
                 .end((err, res) => {
-                    res.should.have.status(422);
+                    res.should.have.status(404);
                     done();
                 });
         });
 
         it('it should reject with no title provided', done => {
-            let recipe = create_valid_recipe();
+            let valid_user = create_valid_user();
+            let recipe = create_valid_recipe(valid_user.facebookID);
             delete recipe.title;
 
-            chai.request(server)
-                .post('/recipe/create')
-                .send(recipe)
-                .end((err, res) => {
-                    res.should.have.status(422);
-                    done();
-                });
+            User.create(valid_user, (err, res) => {
+                chai.request(server)
+                    .post('/recipe/create')
+                    .send(recipe)
+                    .end((err, res) => {
+                        res.should.have.status(422);
+                        done();
+                    });
+            })
+
         });
 
         it('it should reject without ingredient name provided', done => {
-            let recipe = create_valid_recipe();
+            
+            let valid_user = create_valid_user();
+            let recipe = create_valid_recipe(valid_user.facebookID);
             delete recipe.ingredients[0]['name'];
 
-            chai.request(server)
-                .post('/recipe/create')
-                .send(recipe)
-                .end((err, res) => {
-                    res.should.have.status(422);
-                    done();
-                });
+            User.create(valid_user, (err, res) => {
+                chai.request(server)
+                    .post('/recipe/create')
+                    .send(recipe)
+                    .end((err, res) => {
+                        res.should.have.status(422);
+                        done();
+                    });
+            })
+            
         });
 
         it('it should reject without ingredient amount provided', done => {
-            let recipe = create_valid_recipe();
+
+            let valid_user = create_valid_user();
+            let recipe = create_valid_recipe(valid_user.facebookID);
             delete recipe.ingredients[0]['amount'];
+            
+            User.create(valid_user, (err, res) => {
+                chai.request(server)
+                    .post('/recipe/create')
+                    .send(recipe)
+                    .end((err, res) => {
+                        res.should.have.status(422);
+                        done();
+                    });
+            })
+            
+        });
+
+        it('it should succeed with valid information', done => {
+            let valid_user = create_valid_user();
+            let recipe = create_valid_recipe(valid_user.facebookID);
+            
+            User.create(valid_user, (err, res) => {
+                chai.request(server)
+                    .post('/recipe/create')
+                    .send(recipe)
+                    .end((err, res) => {
+                        res.should.have.status(201);
+                        done();
+                    });
+            })
+        });
+      });
+
+    describe('/GET Recipe by ID', () => {
+
+
+        it('it should reject an invalid ID', done => {
 
             chai.request(server)
-                .post('/recipe/create')
-                .send(recipe)
+                .get('/recipe/recipe-byid/' + 'invalid')
                 .end((err, res) => {
                     res.should.have.status(422);
                     done();
                 });
         });
 
-        it('it should succeed with valid information', done => {
-            let recipe = create_valid_recipe();
+        it('it should reject an ID that is not created', done => {
 
             chai.request(server)
-                .post('/recipe/create')
-                .send(recipe)
+                .get('/recipe/recipe-byid/' + mongoose.Types.ObjectId())
                 .end((err, res) => {
-                    res.should.have.status(201);
+                    res.should.have.status(404);
                     done();
                 });
         });
-      });
 
-      describe('/GET Recipe by ID', () => {
+        it('it should accept with a valid recipe id', done => {
+            let recipe = create_valid_recipe('anyID');
 
-          it('it should reject an ID that is not created', done => {
-              let recipe = create_valid_recipe();
-
-              chai.request(server)
-                  .post('/recipe/' + recipe._id)
-                  .send(recipe)
-                  .end((err, res) => {
-                      res.should.have.status(404);
-                      done();
-                  });
-          });
+            Recipe.create(recipe, (err, recipe2) => {
+                chai.request(server)
+                    .get('/recipe/recipe-byid/' + recipe2._id)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        done();
+                    });
+                });
         });
+    });
+
+    describe('/GET Recipe by Owner ID', () => {
+
+        it('it should reject an owner ID that does not exists', done => {
+
+            chai.request(server)
+                .get('/recipe/by-owner/' + 'invalid')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+
+        it('it should accept with a valid recipe id', done => {
+            let valid_user = create_valid_user('anyID');
+
+            User.create(valid_user, (err, user) => {
+                chai.request(server)
+                    .get('/recipe/by-owner/' + valid_user.facebookID)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.eql([])
+                        done();
+                    });
+                });
+
+        });
+    });
 });
