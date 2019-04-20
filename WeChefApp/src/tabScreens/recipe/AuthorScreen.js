36f30bf99a8c543076856d494390dd628dd8a6f6
Unknown <meng46@purdue.edu>
2019-04-20 17:16:47 -0400

    completed frontend of recipe author screen

--- WeChefApp/src/tabScreens/recipe/AuthorScreen.js
+++ WeChefApp/src/tabScreens/recipe/AuthorScreen.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet,View,Text,StatusBar,TouchableOpacity,Image,Dimensions} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+const image = 'http://www.getmdl.io/assets/demos/welcome_card.jpg';
 
 class AuthorScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      imageSource: image,
+      author: "Monika",
     };
   }
 
@@ -27,7 +26,26 @@ class AuthorScreen extends Component {
 
   render() {
     return (
-      <View>
+      <View style={StyleSheet.container}>
+        <View style={styles.titleHeaderContainer}>
+          <Text style={styles.headerTitle}>Author Profile</Text>
+        </View>
+
+        <View style={styles.profileContainer}>
+          <TouchableOpacity disabled>
+            <Image
+              source={{uri: this.state.imageSource}}
+              style={styles.avatarStyle}
+            />
+          </TouchableOpacity>
+
+          <Text
+            style={styles.nameStyle}
+            onPress={() => this.changeUsername()}
+          >
+            {this.state.author}
+          </Text>
+        </View>
 
       </View>
     );
@@ -35,3 +53,44 @@ class AuthorScreen extends Component {
 }
 
 export default AuthorScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  titleHeaderContainer: {
+    marginTop: getStatusBarHeight() - 20,
+    marginLeft: 25,
+    marginRight: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    color: '#3C3C3C',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  profileContainer: {
+    justifyContent: 'flex-start',
+    //flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatarStyle: {
+    marginTop: 20,
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    backgroundColor: '#D2D2D2',
+  },
+
+  nameStyle: {
+    marginTop: 20,
+    fontFamily: 'Poppins',
+    fontSize: 30,
+    fontWeight: '600',
+    color: '#3C3C3C',
+  },
+
+});
