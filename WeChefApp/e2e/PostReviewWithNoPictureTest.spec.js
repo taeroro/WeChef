5e7fe3facd91a84253e7f9fe411f6bb488ea0697
Unknown <schewshu@purdue.edu>
2019-04-24 23:48:04 -0400

    Improve alert message

---
+++ WeChefApp/e2e/PostReviewWithNoPictureTest.spec.js
@@ -0,0 +1,22 @@
+describe('post review', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have five tabs', async () => {
+    await expect(element(by.text('Recipe'))).toBeVisible();
+    await expect(element(by.text('Saved'))).toBeVisible();
+    await expect(element(by.text('List'))).toBeVisible();
+    await expect(element(by.text('Tools'))).toBeVisible();
+    await expect(element(by.text('Profile'))).toBeVisible();
+  });
+
+  it('should show profile screen and profile setting after tap', async () => {
+    await element(by.text('Profile')).tap();
+
+    await element(by.id('profile_page_setting_button')).tap();
+    await expect(element(by.text('Log Out'))).toBeVisible();
+  });
+
+
+});
