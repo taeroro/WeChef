4a8ddc497957aab223dd13c7aa8e49711759ce0e
Unknown <schewshu@purdue.edu>
2019-04-24 23:45:13 -0400

    remove comment

--- WeChefApp/e2e/postReviewTest.spec.js
+++ WeChefApp/e2e/postReviewTest.spec.js
@@ -18,15 +18,5 @@ describe('post review', () => {
     await expect(element(by.text('Log Out'))).toBeVisible();
   });
 
-/*
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
-  */
 });
